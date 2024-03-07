4f8401d1e4d313418d53de7d166aa4264a127751
pselikhov <pselikhov@diasoft.ru>
2024-03-07 10:24:10 +0300

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
-  {path: '', component: TableComponent}
+  {path: 'table', component: TableComponent}
 ];
 
 @NgModule({
